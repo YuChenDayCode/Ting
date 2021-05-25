b2f57b08a2a651faddc0624de7f104379f6b78f9
chenzhen <yuchen_cz@163.com>
2021-05-25 15:47:50 +0800

    first

---
+++ Ting/Models/Catalog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ting.Models
+{
+    /// <summary>
+    /// 目录
+    /// </summary>
+    public class Catalog
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string link { get; set; }
+        public string catalogname { get; set; }
+    }
+}
